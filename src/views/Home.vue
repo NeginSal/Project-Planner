0b66553da0c0d80674fd1fb64bd4b4a4783f9c92
NeginSal <neginsaljooghi@gmail.com>
2022-04-18 19:37:21 +0430

    editing homevue and data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{project.title}}</p>
+
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
   components: {},
+  data(){
+    return{
+      projects:[]
+
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  }
 };
 </script>
