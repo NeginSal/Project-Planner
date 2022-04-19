e805894aac155f673d61f95758b6f0b49bba8d7e
NeginSal <neginsaljooghi@gmail.com>
2022-04-19 09:57:34 +0430

    adding single project component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="showDetails=!showDetails">{{ project.title }}</h3>
+    </div>
+    <div v-if="showDetails">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
+  props: ["project"],
+};
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
+}
+h3 {
+  cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,19 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <p>{{project.title}}</p>
-
+        <SingleProject :project="project"/>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue'
 export default {
   name: "Home",
-  components: {},
+  components: {
+    SingleProject
+  },
   data(){
     return{
       projects:[]
