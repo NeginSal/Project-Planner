7d5608caf6dd143822eb08b8adb371985b5bafdb
NeginSal <neginsaljooghi@gmail.com>
2022-04-19 10:23:11 +0430

    using material icons

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="project">
     <div class="actions">
-      <h3 @click="showDetails=!showDetails">{{ project.title }}</h3>
+      <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons"> edit </span>
+        <span class="material-icons"> delete </span>
+        <span class="material-icons"> done </span>
+      </div>
     </div>
     <div v-if="showDetails">
       <p>{{ project.details }}</p>
@@ -31,5 +36,19 @@ export default {
 }
 h3 {
   cursor: pointer;
+}
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+.material-icons :hover {
+  color: #777;
 }
 </style>
