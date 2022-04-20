f49c6ebc7486d9e4134fa136e9ac60d4ee455f40
NeginSal <neginsaljooghi@gmail.com>
2022-04-20 11:16:01 +0430

    deleting projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project"/>
+        <SingleProject :project="project" @delete="handleDelete" />
       </div>
     </div>
   </div>
@@ -11,21 +11,25 @@
 <script>
 import SingleProject from '../components/SingleProject.vue'
 export default {
-  name: "Home",
-  components: {
-    SingleProject
+  name: 'Home',
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
   },
-  data(){
-    return{
-      projects:[]
-
-    }
-  },
-  mounted(){
+  mounted() {
     fetch('http://localhost:3000/projects')
-    .then(res => res.json())
-    .then(data => this.projects = data)
-    .catch(err => console.log(err.message))
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter(project => {
+        return project.id !== id
+      })
+    }
   }
-};
+}
 </script>
