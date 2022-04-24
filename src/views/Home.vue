f40403da56df97123f704bb2da0cfa71b591a7b7
NeginSal <neginsaljooghi@gmail.com>
2022-04-24 12:15:16 +0430

    creating a filter nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,47 @@
+
+  
 <template>
   <div class="home">
+    <FilterNav :current="current" @filterChange="current = $event"/>
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
+      <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import SingleProject from "../components/SingleProject.vue";
+import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 export default {
-  name: "Home",
-  components: { SingleProject},
+  name: 'Home',
+  components: { SingleProject, FilterNav },
   data() {
     return {
       projects: [],
+      current: 'all',
     };
   },
   mounted() {
-    fetch("http://localhost:3000/projects")
-      .then((res) => res.json())
-      .then((data) => (this.projects = data))
-      .catch((err) => console.log(err));
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err))
   },
   methods: {
     handleDelete(id) {
-      this.projects = this.projects.filter((project) => {
-        return project.id !== id;
-      });
+      this.projects = this.projects.filter(project => {
+        return project.id !== id
+      })
     },
     handleComplete(id) {
-      let p = this.projects.find((project) => {
-        return project.id === id;
-      });
-      p.complete = !p.complete;
+      let p = this.projects.find(project => {
+        return project.id === id
+      })
+      p.complete = !p.complete 
       // console.log(p)
-    },
-  },
-};
+    }
+  }
+}
 </script>
